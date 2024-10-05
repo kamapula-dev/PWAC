9e784cb756a0de9625a1010cfe55c6849ac98c49
Andrei Niadvetsi <andrew.n@5ly.co>
2024-10-05 03:53:27 +0300

    run pipeline changes

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -5,16 +5,15 @@ import { User } from '../../schemas/user.schema';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Get()
-  async getAllUsers(): Promise<User[]> {
-    return this.userService.findAll();
-  }
-
   @Post()
   async createUser(
     @Body() body: { name: string; email: string },
   ): Promise<User> {
     return this.userService.create(body.name, body.email);
+  }
+
+  @Get()
+  async getAllUsers(): Promise<User[]> {
+    return this.userService.findAll();
   }
 }
