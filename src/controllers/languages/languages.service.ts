587482bbc0fe3969d35adb07aa4b557dbe211091
Aliaksandr Hurynovich <aliaksandr@atomatiq.io>
2024-11-28 22:17:16 +0100

    added Logger to language.service.ts

--- src/controllers/languages/languages.service.ts
+++ src/controllers/languages/languages.service.ts
@@ -3,6 +3,7 @@ import * as path from 'path';
 import { promises as fs } from 'fs';
 import * as deepl from 'deepl-node';
 import * as appRoot from 'app-root-path';
+import { Logger } from '@nestjs/common';
 
 @Injectable()
 export class LanguagesService {
@@ -24,15 +25,15 @@ export class LanguagesService {
     const fallbackFilePath = path.join(this.languagesPath, 'en.json');
 
     try {
-      return await this.readLanguageFile(filePath);
+      return this.readLanguageFile(filePath);
     } catch {
-      console.log(`File ${language}.json not found, falling back to en.json`);
+      Logger.log(`File ${language}.json not found, falling back to en.json`);
     }
 
     try {
-      return await this.readLanguageFile(fallbackFilePath);
+      return this.readLanguageFile(fallbackFilePath);
     } catch {
-      console.log(`File en.json not found, falling back to en.json`);
+      Logger.log(`File en.json not found, falling back to en.json`);
     }
   }
 }
