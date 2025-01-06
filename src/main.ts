cf5026bd06297c255ffdae4e12f1c19a1d42223f
Aliaksandr Hurynovich <aliaksandr@atomatiq.io>
2025-01-06 10:29:37 +0100

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationExceptionFilter } from './services/exception-filter';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,6 +23,9 @@ async function bootstrap() {
       'Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Credentials',
   });
   app.useGlobalFilters(new ValidationExceptionFilter());
+  const config = new DocumentBuilder().setTitle('PWA builder').build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
 
   await app.listen(80);
 }
