2a0ad6e250312b24db8fe09915990981777c8395
Aliaksandr Hurynovich <aliaksandr@atomatiq.io>
2025-01-18 14:28:16 +0100

    updated pwa template

--- pwa-template/src/components/ContentSlider/index.tsx
+++ pwa-template/src/components/ContentSlider/index.tsx
@@ -1,13 +1,48 @@
+import React from "react";
+import YouTube from "react-youtube";
 import { PwaContent } from "../../shared/models";
 
 interface Props {
   pwaContent: PwaContent;
 }
 
+const extractVideoId = (url: string): string | null => {
+  try {
+    const parsedUrl = new URL(url);
+    return parsedUrl.searchParams.get("v");
+  } catch (error) {
+    console.error("Invalid YouTube URL:", error);
+    return null;
+  }
+};
+
 const ContentSlider: React.FC<Props> = ({ pwaContent }) => {
+  const youtubeId = pwaContent.videoUrl
+    ? extractVideoId(pwaContent.videoUrl)
+    : null;
+
   return (
     <div className="mb-6 overflow-x-auto whitespace-nowrap scroll-smooth snap-normal no-scrollbar">
       <div className="flex space-x-4">
+        {youtubeId && (
+          <div
+            style={{ width: "294" }}
+            className="bg-gray-300 overflow-hidden flex-shrink-0 h-[167px] snap-start scrollbar-hide rounded-md"
+          >
+            <YouTube
+              videoId={youtubeId}
+              opts={{
+                width: "294",
+                height: "167",
+                playerVars: {
+                  autoplay: 0,
+                  controls: 1,
+                },
+              }}
+            />
+          </div>
+        )}
+
         {pwaContent.images.map((screen, index) => (
           <div
             key={index}
@@ -17,7 +52,7 @@ const ContentSlider: React.FC<Props> = ({ pwaContent }) => {
             {screen.url && (
               <img
                 src={screen.url}
-                alt="Screen"
+                alt={`Screen-${index}`}
                 className="object-fill w-full h-full rounded-lg"
               />
             )}
