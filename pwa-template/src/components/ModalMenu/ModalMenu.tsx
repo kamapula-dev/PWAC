08c48a2a34a2e4d99cc8f067e93b56664ddc963f
Aliaksandr Hurynovich <aliaksandr@atomatiq.io>
2025-01-23 19:10:34 +0100

    updated ModalMenu

--- pwa-template/src/components/ModalMenu/ModalMenu.tsx
+++ pwa-template/src/components/ModalMenu/ModalMenu.tsx
@@ -1,14 +1,14 @@
-import { Modal } from "antd";
-import { useEffect, useState } from "react";
-import GooglePlayLogo from "../../shared/icons/GooglePlayLogo";
-import { PwaContent, PWAInstallState } from "../../shared/models";
-import { useIntl } from "react-intl";
-import InstallButton from "../InstallButton";
-import { BeforeInstallPromptEvent } from "../../App";
-import { useSelector } from "react-redux";
-import { RootState } from "../../Redux/store/store";
-import { getInstallState } from "../../Redux/feat/InstallSlice";
-import GooglePlayDark from "../../shared/icons/GooglePlayDark";
+import { Modal } from 'antd';
+import { useEffect, useState } from 'react';
+import GooglePlayLogo from '../../shared/icons/GooglePlayLogo';
+import { PwaContent, PWAInstallState } from '../../shared/models';
+import { useIntl } from 'react-intl';
+import InstallButton from '../InstallButton';
+import { BeforeInstallPromptEvent } from '../../App';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../Redux/store/store';
+import { getInstallState } from '../../Redux/feat/InstallSlice';
+import GooglePlayDark from '../../shared/icons/GooglePlayDark';
 
 const ModalMenu = ({
   showAppHeader,
@@ -31,13 +31,20 @@ const ModalMenu = ({
   const intl = useIntl();
 
   const installState = useSelector((state: RootState) =>
-    getInstallState(state.install)
+    getInstallState(state.install),
   );
 
   useEffect(() => {
-    setTimeout(() => {
-      if (PWAInstallState.idle === installState) setIsModalVisible(true);
+    const timer = setTimeout(() => {
+      setIsModalVisible((prev) => {
+        if (PWAInstallState.idle === installState) {
+          return true;
+        }
+        return prev;
+      });
     }, 10000);
+
+    return () => clearTimeout(timer);
   }, [installState]);
 
   return (
@@ -47,29 +54,29 @@ const ModalMenu = ({
       closable
       footer={null}
       width={304}
-      className={dark ? "dark-theme" : ""}
+      className={dark ? 'dark-theme' : ''}
     >
-      <div className={`${dark ? "bg-[#303030]" : "bg-white"}`}>
+      <div className={`${dark ? 'bg-[#303030]' : 'bg-white'}`}>
         <div className={`flex justify-center mb-3 `}>
           {dark ? <GooglePlayDark /> : <GooglePlayLogo />}
         </div>
         <div
           className={`${
-            dark ? "text-[#DFDFDF]" : "text-[#5F6368]"
+            dark ? 'text-[#DFDFDF]' : 'text-[#5F6368]'
           } flex justify-center mb-5`}
         >
           {intl.formatMessage({
-            id: "recommended",
-            defaultMessage: "Recommended by",
+            id: 'recommended',
+            defaultMessage: 'Recommended by',
           })}
           &nbsp;
           <span className="text-[#34A853] font-medium">Google Play</span>
         </div>
         {showAppHeader && (
           <div
-            style={{ boxShadow: "0px 2px 2px 0px #0000003D" }}
+            style={{ boxShadow: '0px 2px 2px 0px #0000003D' }}
             className={`p-4 flex gap-4 items-center rounded-xl mb-4 ${
-              dark ? "bg-[#131313]" : "bg-white"
+              dark ? 'bg-[#131313]' : 'bg-white'
             }`}
           >
             <img
@@ -82,14 +89,14 @@ const ModalMenu = ({
             <div>
               <div
                 className={`font-bold text-base leading-[19px] mb-1 ${
-                  dark ? "text-[#DFDFDF]" : "text-[#020202]"
+                  dark ? 'text-[#DFDFDF]' : 'text-[#020202]'
                 }`}
               >
                 {pwaContent.appName}
               </div>
               <div
                 className={`font-bold text-xs ${
-                  dark ? "text-[#A8C8FB]" : "text-[#1357CD]"
+                  dark ? 'text-[#A8C8FB]' : 'text-[#1357CD]'
                 } leading-[14px] mb-2`}
               >
                 {pwaContent.developerName}
@@ -97,17 +104,17 @@ const ModalMenu = ({
               {pwaContent.hasPaidContentTitle && (
                 <div
                   className={`text-[8px] ${
-                    dark ? "text-[#DFDFDF]" : "text-[#444444]"
+                    dark ? 'text-[#DFDFDF]' : 'text-[#444444]'
                   }`}
                 >
                   {intl.formatMessage({
-                    id: "noAds",
-                    defaultMessage: "No ads",
+                    id: 'noAds',
+                    defaultMessage: 'No ads',
                   })}
                   &nbsp;
                   {intl.formatMessage({
-                    id: "noPaidContent",
-                    defaultMessage: "No paid content",
+                    id: 'noPaidContent',
+                    defaultMessage: 'No paid content',
                   })}
                 </div>
               )}
@@ -116,14 +123,14 @@ const ModalMenu = ({
         )}
         <div
           className={`${
-            dark ? "text-[#DFDFDF]" : "text-[#49454F]"
+            dark ? 'text-[#DFDFDF]' : 'text-[#49454F]'
           } text-sm font-bold text-center mb-2`}
         >
           {title}
         </div>
         <div
           className={`${
-            dark ? "text-[#DFDFDF]" : "text-[#49454F]"
+            dark ? 'text-[#DFDFDF]' : 'text-[#49454F]'
           } mb-5 text-center`}
         >
           {content}
