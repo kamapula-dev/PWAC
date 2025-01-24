c95ee4ff373e6480890000683f0dd86fb432bba3
Aliaksandr Hurynovich <aliaksandr@atomatiq.io>
2025-01-24 16:27:27 +0100

    fixed fbc, fbp

--- pwa-template/src/App.tsx
+++ pwa-template/src/App.tsx
@@ -1,30 +1,31 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect, useState } from 'react';
-import MainView from './components/MainView';
-import AboutView from './components/AboutView';
-import PwaView from './components/PwaView';
-import ReviewsView from './components/ReviewsView';
-import axios from 'axios';
-import { PwaContent } from './shared/models';
-import playMarket from './shared/icons/playMarketIcon.svg';
-import Menu from './components/Menu/Menu';
+import { useEffect, useState } from "react";
+import MainView from "./components/MainView";
+import AboutView from "./components/AboutView";
+import PwaView from "./components/PwaView";
+import ReviewsView from "./components/ReviewsView";
+import axios from "axios";
+import { PwaContent } from "./shared/models";
+import playMarket from "./shared/icons/playMarketIcon.svg";
+import Menu from "./components/Menu/Menu";
 import {
   buildAppLink,
   getExternalId,
   logEvent,
   trackExternalId,
-} from './shared/helpers/analytics.ts';
-import ModalMenu from './components/ModalMenu/ModalMenu.tsx';
+} from "./shared/helpers/analytics.ts";
+import ModalMenu from "./components/ModalMenu/ModalMenu.tsx";
+import Cookies from "js-cookie";
 
 declare const window: any;
 
 export interface BeforeInstallPromptEvent extends Event {
   prompt: () => Promise<void>;
-  userChoice: Promise<{ outcome: 'accepted' | 'dismissed'; platform: string }>;
+  userChoice: Promise<{ outcome: "accepted" | "dismissed"; platform: string }>;
 }
 
 export default function App() {
-  const [view, setView] = useState('main');
+  const [view, setView] = useState("main");
   const [isPWAActive, setIsPWAActive] = useState(false);
   const [pwaContent, setPwaContent] = useState<PwaContent | null>(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -34,20 +35,31 @@ export default function App() {
 
   useEffect(() => {
     window.addEventListener(
-      'beforeinstallprompt',
+      "beforeinstallprompt",
       (e: BeforeInstallPromptEvent) => {
         e.preventDefault();
-        console.log('beforeinstallprompt fired');
+        console.log("beforeinstallprompt fired");
         setInstallPrompt(e);
-      },
+      }
     );
 
     return () => {
-      window.removeEventListener('beforeinstallprompt', () => {
+      window.removeEventListener("beforeinstallprompt", () => {
         setInstallPrompt(null);
       });
     };
   }, []);
+
+  useEffect(() => {
+    if (!isPWAActive) {
+      setTimeout(() => {
+        const fbc = Cookies.get("_fbc");
+        const fbp = Cookies.get("_fbp");
+        localStorage.setItem("fbc", fbc || "");
+        localStorage.setItem("fbp", fbp || "");
+      }, 3000);
+    }
+  }, [isPWAActive]);
 
   useEffect(() => {
     if (isPWAActive) return;
@@ -56,10 +68,10 @@ export default function App() {
         const response = await axios.get(
           `https://pwac.world/pwa-content/${
             import.meta.env.VITE_PWA_CONTENT_ID
-          }/trusted`,
+          }/trusted`
         );
 
-        const language = navigator.language.split('-')[0];
+        const language = navigator.language.split("-")[0];
 
         const pwaContent = {
           ...response.data,
@@ -106,16 +118,16 @@ export default function App() {
 
         if (window.matchMedia && !!pwaContent?.theme?.auto) {
           const darkModeMediaQuery = window.matchMedia(
-            '(prefers-color-scheme: dark)',
+            "(prefers-color-scheme: dark)"
           );
 
           setDark(darkModeMediaQuery.matches);
 
-          if (typeof darkModeMediaQuery.addEventListener === 'function') {
-            darkModeMediaQuery.addEventListener('change', (event: any) => {
+          if (typeof darkModeMediaQuery.addEventListener === "function") {
+            darkModeMediaQuery.addEventListener("change", (event: any) => {
               setDark(event.matches);
             });
-          } else if (typeof darkModeMediaQuery.addListener === 'function') {
+          } else if (typeof darkModeMediaQuery.addListener === "function") {
             darkModeMediaQuery.addListener((event: any) => {
               setDark(event.matches);
             });
@@ -125,17 +137,17 @@ export default function App() {
         }
 
         if (window.fbq && pwaContent?.pixel?.length) {
-          const eventName = 'OpenPage';
+          const eventName = "OpenPage";
           let viewContentEvent;
 
           pwaContent.pixel.forEach((pixel) => {
             const event = pixel.events.find(
-              ({ triggerEvent }) => triggerEvent === eventName,
+              ({ triggerEvent }) => triggerEvent === eventName
             );
 
             if (event) {
               viewContentEvent = eventName;
-              window.fbq('track', pixel.pixelId, event.sentEvent);
+              window.fbq("track", pixel.pixelId, event.sentEvent);
             }
           });
           if (viewContentEvent && pwaContent._id) {
@@ -143,7 +155,7 @@ export default function App() {
               pwaContent._id,
               window.location.hostname,
               viewContentEvent,
-              getExternalId(),
+              getExternalId()
             );
           }
         }
@@ -171,7 +183,7 @@ export default function App() {
 
   useEffect(() => {
     const isPWAActivated = window.matchMedia(
-      '(display-mode: standalone)',
+      "(display-mode: standalone)"
     ).matches;
 
     setIsPWAActive(isPWAActivated);
@@ -182,7 +194,7 @@ export default function App() {
       }${
         window.location.search
       }#Intent;scheme=https;package=com.android.chrome;S.browser_fallback_url=${encodeURIComponent(
-        window.location.href,
+        window.location.href
       )};end`;
 
       window.location.href = intentUrl;
@@ -194,7 +206,7 @@ export default function App() {
   let currentView;
 
   switch (view) {
-    case 'main':
+    case "main":
       currentView = (
         <MainView
           dark={dark}
@@ -204,12 +216,12 @@ export default function App() {
         />
       );
       break;
-    case 'about':
+    case "about":
       currentView = (
         <AboutView dark={dark} setView={setView} pwaContent={pwaContent} />
       );
       break;
-    case 'reviews':
+    case "reviews":
       currentView = (
         <ReviewsView dark={dark} pwaContent={pwaContent} setView={setView} />
       );
@@ -217,7 +229,13 @@ export default function App() {
   }
 
   return isPWAActive ? (
-    <PwaView pwaLink={buildAppLink(pwaContent?.pwaLink)} />
+    <PwaView
+      pwaLink={buildAppLink(
+        pwaContent?.pwaLink,
+        localStorage.getItem("fbc"),
+        localStorage.getItem("fbp")
+      )}
+    />
   ) : (
     <div>
       {dark && <style>{`body{background-color: #131313;}`}</style>}
@@ -225,12 +243,12 @@ export default function App() {
         style={
           dark
             ? {
-                background: 'rgb(19, 19, 19)',
+                background: "rgb(19, 19, 19)",
               }
             : {}
         }
         className={`fixed z-[10000000] bg-white w-full h-full justify-center items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${
-          isLoading && pwaContent?.hasLoadingScreen ? 'flex' : 'hidden'
+          isLoading && pwaContent?.hasLoadingScreen ? "flex" : "hidden"
         }`}
       >
         <img src={playMarket} className="w-[125px] h-[137px]" />
--- pwa-template/src/shared/helpers/analytics.ts
+++ pwa-template/src/shared/helpers/analytics.ts
@@ -1,84 +1,84 @@
-import { v4 as uuidv4 } from 'uuid';
-import Cookies from 'js-cookie';
+import { v4 as uuidv4 } from "uuid";
+import Cookies from "js-cookie";
 
 function generateRandomEmail() {
   const randomString = Math.random().toString(36).substring(2, 8);
-  const domains = ['gmail.com', 'yahoo.com', 'outlook.com', 'icloud.com'];
+  const domains = ["gmail.com", "yahoo.com", "outlook.com", "icloud.com"];
   const randomDomain = domains[Math.floor(Math.random() * domains.length)];
   return `${randomString}@${randomDomain}`;
 }
 
 function generateRandomPhoneNumber() {
   const randomDigits = Array.from({ length: 10 }, () =>
-    Math.floor(Math.random() * 10),
-  ).join('');
+    Math.floor(Math.random() * 10)
+  ).join("");
   return `1${randomDigits}`;
 }
 
 function generateRandomName() {
   const firstNames = [
-    'John',
-    'Jane',
-    'Alice',
-    'Bob',
-    'Charlie',
-    'Eve',
-    'Frank',
-    'Grace',
-    'Hannah',
-    'Ivy',
-    'Jack',
-    'Karen',
-    'Liam',
-    'Mia',
-    'Noah',
-    'Olivia',
-    'Paul',
-    'Quinn',
-    'Ruby',
-    'Sophia',
-    'Thomas',
-    'Uma',
-    'Violet',
-    'William',
-    'Xander',
-    'Yara',
-    'Zoe',
-    'Emma',
-    'Lily',
-    'James',
+    "John",
+    "Jane",
+    "Alice",
+    "Bob",
+    "Charlie",
+    "Eve",
+    "Frank",
+    "Grace",
+    "Hannah",
+    "Ivy",
+    "Jack",
+    "Karen",
+    "Liam",
+    "Mia",
+    "Noah",
+    "Olivia",
+    "Paul",
+    "Quinn",
+    "Ruby",
+    "Sophia",
+    "Thomas",
+    "Uma",
+    "Violet",
+    "William",
+    "Xander",
+    "Yara",
+    "Zoe",
+    "Emma",
+    "Lily",
+    "James",
   ];
   const lastNames = [
-    'Smith',
-    'Johnson',
-    'Williams',
-    'Jones',
-    'Brown',
-    'Davis',
-    'Miller',
-    'Wilson',
-    'Moore',
-    'Taylor',
-    'Anderson',
-    'Thomas',
-    'Jackson',
-    'White',
-    'Harris',
-    'Martin',
-    'Thompson',
-    'Garcia',
-    'Martinez',
-    'Robinson',
-    'Clark',
-    'Rodriguez',
-    'Lewis',
-    'Lee',
-    'Walker',
-    'Hall',
-    'Allen',
-    'Young',
-    'King',
-    'Scott',
+    "Smith",
+    "Johnson",
+    "Williams",
+    "Jones",
+    "Brown",
+    "Davis",
+    "Miller",
+    "Wilson",
+    "Moore",
+    "Taylor",
+    "Anderson",
+    "Thomas",
+    "Jackson",
+    "White",
+    "Harris",
+    "Martin",
+    "Thompson",
+    "Garcia",
+    "Martinez",
+    "Robinson",
+    "Clark",
+    "Rodriguez",
+    "Lewis",
+    "Lee",
+    "Walker",
+    "Hall",
+    "Allen",
+    "Young",
+    "King",
+    "Scott",
   ];
   const firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
   const lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
@@ -94,17 +94,17 @@ function generateRandomBirthdate() {
   const endYear = currentYear - 22;
   const year =
     Math.floor(Math.random() * (endYear - startYear + 1)) + startYear;
-  const month = String(Math.floor(Math.random() * 12) + 1).padStart(2, '0');
-  const day = String(Math.floor(Math.random() * 28) + 1).padStart(2, '0');
+  const month = String(Math.floor(Math.random() * 12) + 1).padStart(2, "0");
+  const day = String(Math.floor(Math.random() * 28) + 1).padStart(2, "0");
   return `${year}${month}${day}`;
 }
 
 export function getExternalId() {
-  let externalId = localStorage.getItem('external_id');
+  let externalId = localStorage.getItem("external_id");
 
   if (!externalId) {
     externalId = uuidv4();
-    localStorage.setItem('external_id', externalId);
+    localStorage.setItem("external_id", externalId);
   }
 
   return externalId;
@@ -114,26 +114,28 @@ export function getUrlParams() {
   const urlParams = new URLSearchParams(window.location.search);
   const paramsString = urlParams.toString();
 
-  localStorage.setItem('url_params', paramsString);
+  localStorage.setItem("url_params", paramsString);
 
   return paramsString;
 }
 
-export function buildAppLink(appLink: string) {
+export function buildAppLink(
+  appLink: string,
+  fbc: string | null,
+  fbp: string | null
+) {
   const externalId = getExternalId();
   const urlParams = getUrlParams();
-  const fbc = Cookies.get('_fbc');
-  const fbp = Cookies.get('_fbp');
   const domain = window.location.hostname;
 
   const url = new URL(appLink, window.location.origin);
 
   for (const [key, value] of url.searchParams.entries()) {
     if (
-      value.includes('{external_id}') ||
+      value.includes("{external_id}") ||
       value.includes('"{external_id}"') ||
       value.includes("'{external_id}'") ||
-      value.includes('`{external_id}`')
+      value.includes("`{external_id}`")
     ) {
       url.searchParams.set(
         key,
@@ -141,13 +143,13 @@ export function buildAppLink(appLink: string) {
           .replace(/\{external_id\}/g, externalId)
           .replace(/"\{external_id\}"/g, externalId)
           .replace(/'\{external_id\}'/g, externalId)
-          .replace(/`\{external_id\}`/g, externalId),
+          .replace(/`\{external_id\}`/g, externalId)
       );
     } else if (
-      value.includes('{domain}') ||
+      value.includes("{domain}") ||
       value.includes('"{domain}"') ||
       value.includes("'{domain}'") ||
-      value.includes('`{domain}`')
+      value.includes("`{domain}`")
     ) {
       url.searchParams.set(
         key,
@@ -155,35 +157,35 @@ export function buildAppLink(appLink: string) {
           .replace(/\{domain\}/g, domain)
           .replace(/"\{domain\}"/g, domain)
           .replace(/'\{domain\}'/g, domain)
-          .replace(/`\{domain\}`/g, domain),
+          .replace(/`\{domain\}`/g, domain)
       );
     } else if (
-      value.includes('{fbp}') ||
+      value.includes("{fbp}") ||
       value.includes('"{fbp}"') ||
       value.includes("'{fbp}'") ||
-      value.includes('`{fbp}`')
+      value.includes("`{fbp}`")
     ) {
       url.searchParams.set(
         key,
         value
-          .replace(/\{fbp\}/g, fbp || '')
-          .replace(/"\{fbp\}"/g, fbp || '')
-          .replace(/'\{fbp\}'/g, fbp || '')
-          .replace(/`\{fbp\}`/g, fbp || ''),
+          .replace(/\{fbp\}/g, fbp || "")
+          .replace(/"\{fbp\}"/g, fbp || "")
+          .replace(/'\{fbp\}'/g, fbp || "")
+          .replace(/`\{fbp\}`/g, fbp || "")
       );
     } else if (
-      value.includes('{fbc}') ||
+      value.includes("{fbc}") ||
       value.includes('"{fbc}"') ||
       value.includes("'{fbc}'") ||
-      value.includes('`{fbc}`')
+      value.includes("`{fbc}`")
     ) {
       url.searchParams.set(
         key,
         value
-          .replace(/\{fbc\}/g, fbc || '')
-          .replace(/"\{fbc\}"/g, fbc || '')
-          .replace(/'\{fbc\}'/g, fbc || '')
-          .replace(/`\{fbc\}`/g, fbc || ''),
+          .replace(/\{fbc\}/g, fbc || "")
+          .replace(/"\{fbc\}"/g, fbc || "")
+          .replace(/'\{fbc\}'/g, fbc || "")
+          .replace(/`\{fbc\}`/g, fbc || "")
       );
     }
   }
@@ -207,11 +209,11 @@ export async function trackExternalId(pwaId: string) {
   let country = null;
 
   try {
-    const ipResponse = await fetch('https://api.ipify.org?format=json');
+    const ipResponse = await fetch("https://api.ipify.org?format=json");
     const ipData = await ipResponse.json();
     ip = ipData.ip;
   } catch (error) {
-    console.warn('Failed to retrieve IP:', error);
+    console.warn("Failed to retrieve IP:", error);
   }
 
   try {
@@ -221,16 +223,16 @@ export async function trackExternalId(pwaId: string) {
       country = geoData.country_name;
     }
   } catch (error) {
-    console.warn('Failed to retrieve country:', error);
+    console.warn("Failed to retrieve country:", error);
   }
 
   try {
     const { firstName, lastName } = generateRandomName();
 
-    const response = await fetch('https://pwac.world/pwa-external-mapping', {
-      method: 'POST',
+    const response = await fetch("https://pwac.world/pwa-external-mapping", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         externalId: externalId,
@@ -244,21 +246,21 @@ export async function trackExternalId(pwaId: string) {
         email: generateRandomEmail(),
         dob: generateRandomBirthdate(),
         userAgent: navigator.userAgent,
-        fbp: Cookies.get('_fbp'),
-        fbc: Cookies.get('_fbc'),
+        fbp: Cookies.get("_fbp"),
+        fbc: Cookies.get("_fbc"),
       }),
     });
 
     if (!response.ok) {
       console.error(
-        'Failed to save external_id mapping:',
-        await response.text(),
+        "Failed to save external_id mapping:",
+        await response.text()
       );
     } else {
-      console.log('external_id mapping saved successfully.');
+      console.log("external_id mapping saved successfully.");
     }
   } catch (error) {
-    console.error('Error while saving external_id mapping:', error);
+    console.error("Error while saving external_id mapping:", error);
   }
 }
 
@@ -268,13 +270,13 @@ export async function logEvent(
   event: string,
   externalId?: string | null,
   value?: number,
-  currency?: string,
+  currency?: string
 ) {
   try {
-    const response = await fetch('https://pwac.world/pwa-event-log', {
-      method: 'POST',
+    const response = await fetch("https://pwac.world/pwa-event-log", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         pwaContentId,
@@ -287,11 +289,11 @@ export async function logEvent(
     });
 
     if (!response.ok) {
-      console.error('Failed to log event:', await response.text());
+      console.error("Failed to log event:", await response.text());
     } else {
-      console.log('Event logged successfully.');
+      console.log("Event logged successfully.");
     }
   } catch (error) {
-    console.error('Error while logging event:', error);
+    console.error("Error while logging event:", error);
   }
 }
