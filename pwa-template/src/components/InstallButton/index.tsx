68ba62144fa7508c7e5c23965402a1fad980e1aa
Aliaksandr Hurynovich <aliaksandr@atomatiq.io>
2025-02-02 02:05:13 +0100

    reduced time of downloading pwa

--- pwa-template/src/components/InstallButton/index.tsx
+++ pwa-template/src/components/InstallButton/index.tsx
@@ -60,7 +60,7 @@ const InstallButton: React.FC<Props> = ({
         console.log('User accepted installation');
         setTimeout(() => {
           dispatch(setInstallState(PWAInstallState.installed));
-        }, 70000);
+        }, 12000);
 
         if (window.fbq) {
           if (pixel?.length) {
