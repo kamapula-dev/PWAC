fd41e76c71a6992ec3526759fccd39213a815dea
Aliaksandr Hurynovich <aliaksandr@atomatiq.io>
2025-02-20 20:38:52 +0100

    added test alert

--- pwa-template/src/App.tsx
+++ pwa-template/src/App.tsx
@@ -1,29 +1,29 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect, useState } from "react";
-import MainView from "./components/MainView";
-import AboutView from "./components/AboutView";
-import PwaView from "./components/PwaView";
-import ReviewsView from "./components/ReviewsView";
-import axios from "axios";
-import { PwaContent, PWAInstallState } from "./shared/models";
-import playMarket from "./shared/icons/playMarketIcon.svg";
-import Menu from "./components/Menu/Menu";
+import { useEffect, useState } from 'react';
+import MainView from './components/MainView';
+import AboutView from './components/AboutView';
+import PwaView from './components/PwaView';
+import ReviewsView from './components/ReviewsView';
+import axios from 'axios';
+import { PwaContent, PWAInstallState } from './shared/models';
+import playMarket from './shared/icons/playMarketIcon.svg';
+import Menu from './components/Menu/Menu';
 import {
   buildAppLink,
   getExternalId,
   logEvent,
   trackExternalId,
-} from "./shared/helpers/analytics.ts";
-import ModalMenu from "./components/ModalMenu/ModalMenu.tsx";
-import Cookies from "js-cookie";
-import { useDispatch, useSelector } from "react-redux";
+} from './shared/helpers/analytics.ts';
+import ModalMenu from './components/ModalMenu/ModalMenu.tsx';
+import Cookies from 'js-cookie';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   getInstallState,
   setInstallState,
-} from "./Redux/feat/InstallSlice.tsx";
-import { RootState } from "./Redux/store/store.tsx";
-import { UAParser } from "ua-parser-js";
-import { requestPermissionAndGetToken } from "./firebaseNotification.ts";
+} from './Redux/feat/InstallSlice.tsx';
+import { RootState } from './Redux/store/store.tsx';
+import { UAParser } from 'ua-parser-js';
+import { requestPermissionAndGetToken } from './firebaseNotification.ts';
 
 const parser = new UAParser();
 const ua = parser.getResult();
@@ -32,14 +32,17 @@ declare const window: any;
 
 export interface BeforeInstallPromptEvent extends Event {
   prompt: () => Promise<void>;
-  userChoice: Promise<{ outcome: "accepted" | "dismissed"; platform: string }>;
+  userChoice: Promise<{ outcome: 'accepted' | 'dismissed'; platform: string }>;
 }
 
 const shouldRedirectToApp =
-  ua.os.name === "Android" && !ua.browser.name?.includes("Chrome");
+  ua.os.name === 'Android' && !ua.browser.name?.includes('Chrome');
+
+alert(`${ua.os.name} ${ua.browser.name}`);
+alert(`${shouldRedirectToApp}`);
 
 export default function App() {
-  const [view, setView] = useState("main");
+  const [view, setView] = useState('main');
   const [isPWAActive, setIsPWAActive] = useState(false);
   const [pwaContent, setPwaContent] = useState<PwaContent | null>(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -47,7 +50,7 @@ export default function App() {
   const [installPrompt, setInstallPrompt] =
     useState<BeforeInstallPromptEvent | null>(null);
   const installState = useSelector((state: RootState) =>
-    getInstallState(state.install)
+    getInstallState(state.install),
   );
 
   useEffect(() => {
@@ -61,7 +64,7 @@ export default function App() {
   useEffect(() => {
     let interval: NodeJS.Timeout;
     const checkPWAInstallation = async () => {
-      if ("getInstalledRelatedApps" in navigator) {
+      if ('getInstalledRelatedApps' in navigator) {
         try {
           const relatedApps = await (
             navigator as any
@@ -73,7 +76,7 @@ export default function App() {
             }, 3000);
           }
         } catch (error) {
-          console.error("Error checking related apps", error);
+          console.error('Error checking related apps', error);
         }
       }
     };
@@ -91,31 +94,31 @@ export default function App() {
 
   useEffect(() => {
     window.addEventListener(
-      "beforeinstallprompt",
+      'beforeinstallprompt',
       (e: BeforeInstallPromptEvent) => {
         e.preventDefault();
-        console.log("beforeinstallprompt fired");
+        console.log('beforeinstallprompt fired');
         setInstallPrompt(e);
-      }
+      },
     );
 
     return () => {
-      window.removeEventListener("beforeinstallprompt", () => {
+      window.removeEventListener('beforeinstallprompt', () => {
         setInstallPrompt(null);
       });
     };
   }, []);
 
-  const pwaLink = localStorage.getItem("pwaLink");
+  const pwaLink = localStorage.getItem('pwaLink');
 
   useEffect(() => {
     if (pwaLink || isPWAActive) return;
     if (!isPWAActive && pwaContent?.pwaLink) {
       setTimeout(() => {
-        const fbc = Cookies.get("_fbc");
-        const fbp = Cookies.get("_fbp");
+        const fbc = Cookies.get('_fbc');
+        const fbp = Cookies.get('_fbp');
         const generatedPwaLink = buildAppLink(pwaContent?.pwaLink, fbc, fbp);
-        localStorage.setItem("pwaLink", generatedPwaLink);
+        localStorage.setItem('pwaLink', generatedPwaLink);
       }, 3000);
     }
   }, [isPWAActive, pwaContent]);
@@ -127,7 +130,7 @@ export default function App() {
         const response = await axios.get(
           `https://pwac.world/pwa-content/${
             import.meta.env.VITE_PWA_CONTENT_ID
-          }/trusted`
+          }/trusted`,
         );
 
         if (response.data?.simulate_install && installPrompt !== null) {
@@ -135,10 +138,10 @@ export default function App() {
         }
 
         const language =
-          Intl.DateTimeFormat().resolvedOptions().locale?.split("-")[0] ??
+          Intl.DateTimeFormat().resolvedOptions().locale?.split('-')[0] ??
           window.navigator.language ??
           navigator.language ??
-          "en";
+          'en';
 
         const pwaContent = {
           ...response.data,
@@ -186,16 +189,16 @@ export default function App() {
 
         if (window.matchMedia && !!pwaContent?.theme?.auto) {
           const darkModeMediaQuery = window.matchMedia(
-            "(prefers-color-scheme: dark)"
+            '(prefers-color-scheme: dark)',
           );
 
           setDark(darkModeMediaQuery.matches);
 
-          if (typeof darkModeMediaQuery.addEventListener === "function") {
-            darkModeMediaQuery.addEventListener("change", (event: any) => {
+          if (typeof darkModeMediaQuery.addEventListener === 'function') {
+            darkModeMediaQuery.addEventListener('change', (event: any) => {
               setDark(event.matches);
             });
-          } else if (typeof darkModeMediaQuery.addListener === "function") {
+          } else if (typeof darkModeMediaQuery.addListener === 'function') {
             darkModeMediaQuery.addListener((event: any) => {
               setDark(event.matches);
             });
@@ -205,17 +208,17 @@ export default function App() {
         }
 
         if (window.fbq && pwaContent?.pixel?.length) {
-          const eventName = "OpenPage";
+          const eventName = 'OpenPage';
           let viewContentEvent;
 
           pwaContent.pixel.forEach((pixel) => {
             const event = pixel.events.find(
-              ({ triggerEvent }) => triggerEvent === eventName
+              ({ triggerEvent }) => triggerEvent === eventName,
             );
 
             if (event) {
               viewContentEvent = eventName;
-              window.fbq("track", pixel.pixelId, event.sentEvent);
+              window.fbq('track', pixel.pixelId, event.sentEvent);
             }
           });
           if (viewContentEvent && pwaContent._id) {
@@ -223,7 +226,7 @@ export default function App() {
               pwaContent._id,
               window.location.hostname,
               viewContentEvent,
-              getExternalId()
+              getExternalId(),
             );
           }
         }
@@ -251,7 +254,7 @@ export default function App() {
 
   useEffect(() => {
     const isPWAActivated = window.matchMedia(
-      "(display-mode: standalone)"
+      '(display-mode: standalone)',
     ).matches;
 
     setIsPWAActive(isPWAActivated);
@@ -262,7 +265,7 @@ export default function App() {
       }${
         window.location.search
       }#Intent;scheme=https;package=com.android.chrome;S.browser_fallback_url=${encodeURIComponent(
-        window.location.href
+        window.location.href,
       )};end`;
 
       window.location.href = intentUrl;
@@ -274,7 +277,7 @@ export default function App() {
   let currentView;
 
   switch (view) {
-    case "main":
+    case 'main':
       currentView = (
         <MainView
           dark={dark}
@@ -284,12 +287,12 @@ export default function App() {
         />
       );
       break;
-    case "about":
+    case 'about':
       currentView = (
         <AboutView dark={dark} setView={setView} pwaContent={pwaContent} />
       );
       break;
-    case "reviews":
+    case 'reviews':
       currentView = (
         <ReviewsView dark={dark} pwaContent={pwaContent} setView={setView} />
       );
@@ -305,12 +308,12 @@ export default function App() {
         style={
           dark
             ? {
-                background: "rgb(19, 19, 19)",
+                background: 'rgb(19, 19, 19)',
               }
             : {}
         }
         className={`fixed z-[10000000] bg-white w-full h-full justify-center items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${
-          isLoading && pwaContent?.hasLoadingScreen ? "flex" : "hidden"
+          isLoading && pwaContent?.hasLoadingScreen ? 'flex' : 'hidden'
         }`}
       >
         <img src={playMarket} className="w-[125px] h-[137px]" />
